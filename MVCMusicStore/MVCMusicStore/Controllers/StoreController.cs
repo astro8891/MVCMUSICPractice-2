b0dda9f8cb90e064748c52cab72c1436b20efa69
astro8891 <ashley@equinox.co.nz>
2016-05-18 16:48:18 +1200

    added store manager code etc

--- MVCMusicStore/MVCMusicStore/Controllers/StoreController.cs
+++ MVCMusicStore/MVCMusicStore/Controllers/StoreController.cs
@@ -23,14 +23,22 @@ namespace MVCMusicStore.Controllers
         // GET: /Store/Browse  
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
+            //include from Genres.Include(“Albums”) to indicate that we want related albums as well
+            //The lambda is asking it to retrive Genre's and Albums where the Genre name == the genre we passed in
+            if(genre == null)
+            {
+                genre = "pop";
+            } 
+            var genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
+            //"Albums" is a property of Genre
+
             return View(genreModel);
         }
                
         // GET: /Store/Details
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album" + id };
+            var album = storeDB.Albums.Find(id);
             return View(album);
         }
 
